79d585953cec136ae1f6fc834b6bc74976fc2f48
Dario <dario.mennillo@immaginazioneelavoro.it>
2026-02-20 14:17:58 +0100

    lezione 20/02

---
+++ project0_helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, world !!!");
+		System.out.println("Hello, Dario. Questa è la tua prima lezione di JAVA");
+		
+		//Commenti: vengono bypassati nella lettura del codice. Servono solo a noi sviluppatori
+		
+		//Istanzio (creare) qualche variabile
+		//Tipo nome = valore
+		String nomeCorso = "B22-418-2025 TSPPI";
+		String sede = "Immaginazione e Lavoro";
+		
+		//Richiamo (leggere) la variabile
+		System.out.println("Sto partecipando al corso di " + nomeCorso + " presso " + sede);
+		//utilizzo l'operatore + per concatenare delle variabili
+
+		
+	}
+}
---
+++ project0_helloWorld/src/helloWorld/helloDario.java
@@ -0,0 +1,19 @@
+package helloWorld;
+
+public class helloDario {
+	public static void main(String[] args) {
+	//descrivi te stesso (nome, cognome, età, nazionalità). Stampa tutte queste info in un Syso. Att a variabile età: dichiarala in questo modo --> byte eta = 36;
+		
+		String nome = "Dario";
+		String cognome = "Mennillo";
+		byte eta = 36;
+		String nazionalita = "italiana";
+		String codFisc = "MNNdaoijeekm89kcalk";
+		String indirizzo = "via Tal dei tali 0";
+		String lavoro = "Docente";
+		
+		
+		System.out.println("Ciao, mi chiamo: " + nome + " " + cognome + "\nAltre info:\n" + "codFisc. " + codFisc + "\nIndirizzo: " + indirizzo + "\nLavoro: " + lavoro);
+		
+	}
+}
