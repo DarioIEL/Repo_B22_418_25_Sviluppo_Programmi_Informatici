fa83e9eb96c1bec08ef60b8fdd00093156d9cec2
= <dario.mennillo@immaginazioneelavoro.it>
2026-03-06 14:19:46 +0100

    Update Tombola.java

--- project2_Giochi/src/giochi/Tombola.java
+++ project2_Giochi/src/giochi/Tombola.java
@@ -3,6 +3,6 @@ package giochi;
 public class Tombola {
 // Sviluppa i vari metodi per giocare al gioco della tombola
 //Chiedi all'utente quanti numeri estrarre su un set di 90 numeri
-//Chiedi all'untente quanti numeri estrarre su un set impostato dall'utente stesso
+//Chiedi all'untente quanti numeri estrarre su un set impostato dall'utente stesso (1 - X)
 //ATT: tutte le estrazioni dovranno essere univoche. Non posso estrarre 2 volte lo stesso numero
 }
