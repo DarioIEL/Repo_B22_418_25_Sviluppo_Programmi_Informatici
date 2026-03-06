9f8bac1c5eedcf294ed3079e3d0583150091ca14
= <dario.mennillo@immaginazioneelavoro.it>
2026-03-06 08:37:57 +0100

    Update EsamiUniversita.java

--- LezioniTeoriche/src/lezione02_Statement/EsamiUniversita.java
+++ LezioniTeoriche/src/lezione02_Statement/EsamiUniversita.java
@@ -12,13 +12,48 @@ public class EsamiUniversita {
 		 * all'utente di inserire i voti uno alla volta. Quando sono inseriti entrambi i voti calcola il voto finale (media dei due)
 		 */
 		
+		//CREO Delle costanti utilizzando la parola chiave final
+		final String NOME_APPLICAZIONE = "Inserimento voti Esami";
+		final int VOTO_MAX = 30;
+		final int VOTO_MIN = 0;
+		final int SOGLIA = 18;
+		
+		
+		
 		Scanner scan  = new Scanner(System.in);
 		
-		System.out.println("=== INSERIMENTO VOTO ESAMI ===");
-		
+		System.out.println("===" + NOME_APPLICAZIONE +"===");
 		System.out.println("Caro docente, inserisci il voto dell'esame scritto: ");
 		
 		int votoScritto  = scan.nextInt();
+		
+		if(votoScritto <= VOTO_MAX && votoScritto >= VOTO_MIN) {
+			if(votoScritto >= SOGLIA) {
+				System.out.println("Voto scritto: " + votoScritto);
+				System.out.println("Adesso inserisci il voto dell'orale:");
+				
+				int votoOrale = scan.nextInt();
+				if(votoOrale <= VOTO_MAX && votoOrale >= VOTO_MIN) {
+					
+					if(votoOrale >= 18) {
+						//superato, calcola voto finale
+						System.out.println("Voto Orale: " + votoOrale);
+						double votoFinale = Math.round((votoOrale + votoScritto)/2);
+						System.out.println("Voto Finale: " + votoFinale);
+					}else {
+						//Esame non superato
+						System.out.println("Mi spiace, non hai superato l'esame orale. Quindi no hai superato l'intero esame");
+					}
+					
+				}else {
+					System.out.println("Voto orale non valido");
+				}
+			}else {
+				System.out.println("Mi spiace non hai passato lo scritto. Non puoi accedere all'orale");
+			}
+		}else {
+			System.out.println("Voto inserito non valido");
+		}
 		
 	}
 }
