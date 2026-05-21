ff9a62805dfdbcf66d25406aa2b49424b44d0913
= <dario.mennillo@immaginazioneelavoro.it>
2026-05-21 12:24:48 +0200

    up

--- project11_SpringCore/src/main/java/it/springcore/controller/LibroController.java
+++ project11_SpringCore/src/main/java/it/springcore/controller/LibroController.java
@@ -3,11 +3,18 @@ package it.springcore.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.springcore.dto.LibroRequest;
 import it.springcore.dto.LibroResponse;
 import it.springcore.model.Libro;
 import it.springcore.service.LibroService;
@@ -19,21 +26,39 @@ import it.springcore.service.LibroService;
 
 @RestController
 @CrossOrigin()
+@RequestMapping("/api/libri") //Questo sarà il prefisso comune a tutti i metodi
 public class LibroController {
+	
 	private final LibroService service;
 
 	public LibroController(LibroService service) {
 		this.service = service;
 	}
 	
-	@GetMapping("/libri")
-	public ResponseEntity<List<LibroResponse>> lista(){
+	@GetMapping //Avendo RequestMapping quando chiamo /api/libri chiamerò direttamente il metodo lista
+	public ResponseEntity<List<LibroResponse>> lista(){ //Response Entity è il wrapper integrale della response HTTP. Alò suo interno c'è HEAD, BODY(Json),OPTION, STATUS CODE
 		List<LibroResponse> libri = service.elencoLibri();
 		return ResponseEntity.ok(libri);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<LibroResponse> getLibroSingolo(@PathVariable Long id){
+		LibroResponse libro = service.cercaLibro(id);
+		return ResponseEntity.ok(libro);	
+	}
+	
+	@PostMapping
+	public ResponseEntity<LibroResponse> crea(@RequestBody LibroRequest libroReq){
+		LibroResponse creato = service.aggiungiLibro(libroReq);
+		return ResponseEntity.status(HttpStatus.CREATED).body(creato); //status code della creazione è 201
+	}
 	
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> elimina(@PathVariable Long id){
+		service.eliminaLibro(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
--- project11_SpringCore/web/js/script.js
+++ project11_SpringCore/web/js/script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:8080/libri")
+fetch("http://localhost:8080/api/libri")
 .then(response =>{
     return response.json()
 })
@@ -6,3 +6,35 @@ fetch("http://localhost:8080/libri")
     console.log(data);
     
 })
+
+class Libro{
+    constructor(titolo, autore, prezzo){
+        this.titolo = titolo;
+        this.autore = autore;
+        this.prezzo = prezzo;
+    }
+}
+
+function inserisciLibro(libro){
+
+    let nuovoLibro = new Libro(libro.titolo, libro.autore, libro.prezzo);
+
+    fetch("http://localhost:8080/api/libri", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nuovoLibro)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+
+document.querySelector("#btn").addEventListener("click", ()=>{
+    let libro = new Libro("Baudolino", "Umberto Eco", 13.8);
+    inserisciLibro(libro);
+})
+
+
