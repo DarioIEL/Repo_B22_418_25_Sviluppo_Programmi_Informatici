15d2197eca14cdfdacf9436040d8c1321e824759
= <dario.mennillo@immaginazioneelavoro.it>
2026-05-21 10:00:56 +0200

    up

---
+++ project11_SpringCore/src/main/java/it/springcore/Project11SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package it.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project11SpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project11SpringCoreApplication.class, args);
+	}
+
+}
---
+++ project11_SpringCore/src/test/java/it/springcore/Project10SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project10SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
